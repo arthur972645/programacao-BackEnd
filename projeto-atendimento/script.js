542e4473929cc0419084df68419f877ad55b6377
arthur972645 <arthurall.1973@gmail.com>
2024-03-13 11:52:13 -0300

    entender atendmento

--- projeto-atendimento/script.js
+++ projeto-atendimento/script.js
@@ -60,71 +60,76 @@
 // btnUrgencia.addEventListener('click', adicionarUrgencia)
 // btnAtender.addEventListener('click', atenderPaciente)
 
-const pacientes = []
-// referência para entrada de dados 
+
+//codigo da valquiria
+const pacientes  = []
+
+//Preferência
+
 const inPaciente = document.getElementById('inPaciente')
 
-//Saída de dados
+//Saida de dados
 const outAtendimento = document.getElementById('outAtendimento')
 const outLista = document.getElementById('outLista')
+
 
 //Botões
 const btnAdicionar = document.getElementById('btnAdicionar')
 const btnUrgencia = document.getElementById('btnUrgencia')
 const btnAtender = document.getElementById('btnAtender')
 
-const adicionarPaciente = () =>{
+const adicionarPaciente = () => {
     let nome = inPaciente.value
-    if(nome === ''){
-        alert('Informe o nome do paciente')
+
+    if(nome === ' '){
+        alert(`Informe o nome do paciente`)
         return
     }
-    pacientes.push(nome);
+    pacientes.push(nome)
 
-    let lista = "";
+    let lista = ""
+
     pacientes.map((paciente, index) => {
-        return (lista += `${index+1} - ${paciente}\n`);
-    });
+        return (lista += `${index + 1} - ${paciente}\n`)
+    })
 
     outLista.textContent = lista;
-    inPaciente.value = "";
+    inPaciente.value = ' '
+}
+btnAdicionar.addEventListener("click", adicionarPaciente)
 
-};
-btnAdicionar.addEventListener("click", adicionarPaciente);
-
-const adicionarUrgencia = () =>{
+const adicionarUrgencia = () => {
     let nome = inPaciente.value
-    if(nome === ''){
-        alert('Informe o nome do paciente')
+
+    if(nome === ' '){
+        alert(`Informe o nome do paciente`)
         return
     }
-    pacientes.unshift(nome);
+    pacientes.unshift(nome)
 
-    let lista = "";
+    let lista = ""
+
     pacientes.map((paciente, index) => {
-        return (lista += `${index+1} - ${paciente}\n`);
-    });
+        return (lista += `${index + 1} - ${paciente}\n`)
+    })
 
     outLista.textContent = lista;
-    inPaciente.value = "";
-
-};
-
-btnAdicionar.addEventListener('click', adicionarUrgencia);
+    inPaciente.value = ' '
+}
+btnUrgencia.addEventListener("click", adicionarUrgencia)
 
 const atenderPaciente = () => {
-    if (pacientes.length === 0) {
-        alert("Não Existe paciente na fila de espera");
-        return;
+    if(pacientes.length === 0){
+    alert("Não existe paciente na fila de espera")
+    return 
     }
     let atender = pacientes.shift()
     outAtendimento.textContent = atender
 
-    let lista = "";
     pacientes.map((paciente, index) => {
-        return (lista += `${index+1} - ${paciente}\n`);
-    });
-    
+        return (lista += `${index + 1} - ${paciente}\n`)
+    })
+
     outLista.textContent = lista;
-};
-btnAtender.addEventListener("click", atenderPaciente);
+}
+btnAtender.addEventListener("click", atenderPaciente)
